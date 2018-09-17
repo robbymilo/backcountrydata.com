e44e9286c0239c688f2d2b88f9741af6ecb8fa80
Robby Milo <robbymilo@gmail.com>
2018-09-17 10:13:03 -0600

    fix cors 2

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+
 
 var app = express();
 
