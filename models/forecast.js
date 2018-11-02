36a2e3c533d61c9ffd9c7af1363429b1904f02fe
Robby Milo <robbymilo@gmail.com>
2018-11-02 14:43:48 -0600

    added graphical forecast

--- models/forecast.js
+++ models/forecast.js
@@ -1,9 +1,13 @@
 var axios = require('axios');
 var parser = require('xml2json');
 
+// 43.76377,-116.09685
+// https://forecast.weather.gov/MapClick.php?lat=43.76377&lon=-116.09685&unit=0&lg=english&FcstType=digitalDWML
+
 module.exports = function(req, res, next, lat, lon) {
 	var result = {};
 	var stationLookupUrl = 'https://api.weather.gov/points/' + lat + ',' + lon;
+	var graphicalLookupUrl = 'https://forecast.weather.gov/MapClick.php?lat=' + lat + '&lon=' + lon + '&unit=0&lg=english&FcstType=digitalDWML'
 
 	result['stationLookupUrl'] = stationLookupUrl;
 
@@ -42,8 +46,21 @@ module.exports = function(req, res, next, lat, lon) {
 					axios.get(forecastLookupUrl).then(
 						response => {
 							result['forecast'] = response.data;
+							
+							axios.get(graphicalLookupUrl).then(
+								response => {								
 
-							res.send(result);
+									var forecastGraphical = parser.toJson(response.data, {
+										object: true
+									});
+									result['forecastGraphical'] = forecastGraphical.dwml.data.parameters;		
+									res.send(result);
+								},
+								error => {
+									console.log(error);
+									res.send(error);
+								}
+							);
 						},
 						error => {
 							console.log(error);
