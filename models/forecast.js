ef82a390d2df3acb04aafbb632b8ba5994717d57
Robby Milo <robbymilo@gmail.com>
2018-11-05 10:02:32 -0700

    refactor forecast lookup to async/await

--- models/forecast.js
+++ models/forecast.js
@@ -4,79 +4,67 @@ var parser = require('xml2json');
 // 43.76377,-116.09685
 // https://forecast.weather.gov/MapClick.php?lat=43.76377&lon=-116.09685&unit=0&lg=english&FcstType=digitalDWML
 
-module.exports = function(req, res, next, lat, lon) {
-	var result = {};
-	var stationLookupUrl = 'https://api.weather.gov/points/' + lat + ',' + lon;
-	var graphicalLookupUrl = 'https://forecast.weather.gov/MapClick.php?lat=' + lat + '&lon=' + lon + '&unit=0&lg=english&FcstType=digitalDWML'
+// lookup NWS station so we can retrieve forecast discussion
+async function stationLookup(stationLookupUrl) {
+	return axios({
+		method: "get",
+		url: stationLookupUrl
+	}).then(response => response.data['properties']['forecastOffice']);
+}
 
+// get forecast discussion from NWS
+async function getDiscussion(discussionLookupUrl) {
+	return axios({
+		method: "get",
+		url: discussionLookupUrl
+	}).then(response => response.data);
+}
+
+// get human-readable forecast
+async function getForecast(forecastLookupUrl) {
+	return axios({
+		method: "get",
+		url: forecastLookupUrl
+	}).then(response => response.data);
+}
+
+// get forecast data for charting
+async function getForecastGraphical(graphicalLookupUrl) {
+	return axios({
+		method: "get",
+		url: graphicalLookupUrl
+	}).then(response => response.data);
+}
+
+module.exports = async (req, res, next, lat, lon) => {
+
+	const stationLookupUrl = 'https://api.weather.gov/points/' + lat + ',' + lon;
+	const nwsOfficeURL = await stationLookup(stationLookupUrl);
+	const nwsOffice = nwsOfficeURL.replace('https://api.weather.gov/offices/', '');
+
+	const discussionLookupUrl =	'https://www.wrh.noaa.gov/total_forecast/getprod.php?afos=xxxafd' +	nwsOffice +	'&xml';
+	let discussion = await getDiscussion(discussionLookupUrl);
+	discussion = parser.toJson(discussion, {
+		object: true
+	});
+
+	const forecastLookupUrl = 'https://forecast.weather.gov/MapClick.php?lat=' + lat + '&lon=' + lon + '&FcstType=json';
+	const forecast = await getForecast(forecastLookupUrl);
+
+	const graphicalLookupUrl = 'https://forecast.weather.gov/MapClick.php?lat=' + lat + '&lon=' + lon + '&unit=0&lg=english&FcstType=digitalDWML'
+	let graphicalForecast = await getForecastGraphical(graphicalLookupUrl);
+	graphicalForecast = parser.toJson(graphicalForecast, {
+		object: true
+	});
+
+	const result = {};
 	result['stationLookupUrl'] = stationLookupUrl;
+	result['nwsOffice'] = nwsOfficeURL;
+	result['discussionLookupUrl'] = discussionLookupUrl;
+	result['discussion'] = discussion;
+	result['forecastLookupUrl'] = forecastLookupUrl;
+	result['forecast'] = forecast;
+	result['forecastGraphical'] = graphicalForecast.dwml.data.parameters;
 
-	// find station's forecast office
-	console.log('finding office');
-	axios.get(stationLookupUrl).then(
-		response => {
-			var nwsOffice = response.data['properties']['forecastOffice'];
-			result['nwsOffice'] = nwsOffice;
-			nwsOffice = nwsOffice.replace('https://api.weather.gov/offices/', '');
-
-			var discussionLookupUrl =
-				'https://www.wrh.noaa.gov/total_forecast/getprod.php?afos=xxxafd' +
-				nwsOffice +
-				'&xml';
-			result['discussionLookupUrl'] = discussionLookupUrl;
-
-			console.log('finding discussion');
-			axios.get(discussionLookupUrl).then(
-				response => {
-					var json = parser.toJson(response.data, {
-						object: true
-					});
-
-					result['discussion'] = json;
-
-					var forecastLookupUrl =
-						'https://forecast.weather.gov/MapClick.php?lat=' +
-						lat +
-						'&lon=' +
-						lon +
-						'&FcstType=json';
-					result['forecastLookupUrl'] = forecastLookupUrl;
-
-					console.log('finding forecast');
-					axios.get(forecastLookupUrl).then(
-						response => {
-							result['forecast'] = response.data;
-							
-							axios.get(graphicalLookupUrl).then(
-								response => {								
-
-									var forecastGraphical = parser.toJson(response.data, {
-										object: true
-									});
-									result['forecastGraphical'] = forecastGraphical.dwml.data.parameters;		
-									res.send(result);
-								},
-								error => {
-									console.log(error);
-									res.send(error);
-								}
-							);
-						},
-						error => {
-							console.log(error);
-							res.send(error);
-						}
-					);
-				},
-				error => {
-					console.log(error);
-					res.send(error);
-				}
-			);
-		},
-		error => {
-			console.log(error);
-			res.send(error);
-		}
-	);
+	res.send(result);
 };
