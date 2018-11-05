ca7448faf95d44dfde62c51721c993612e9d7869
Robby Milo <robbymilo@gmail.com>
2018-11-05 12:51:13 -0700

    added nearest avalanche forecast centers

--- controllers/avy_center.js
+++ controllers/avy_center.js
@@ -1,14 +1,65 @@
-var Functions = require('../functions/functions');
-var Station = require('../data/static/station-master.json');
+const geolib = require('geolib');
+var axios = require('axios');
 
-module.exports = avy_center;
+const Functions = require('../functions/functions');
+const Station = require('../data/static/station-master.json');
+const avyRegions = require('../data/static/avy-center-points.json');
 
-function avy_center(req, res, next) {
+// finds nearest avy forecast region
+// returns list of avy regions by distance from center of region vs. station 
+
+async function avyLookup() {
+	return axios({
+		method: "get",
+		url: 'https://avalanche.org/wp-admin/admin-ajax.php?action=map_layer'
+	}).then(response => response.data);
+}
+
+module.exports = async (req, res, next) => {
 	console.log('finding station avy center');
 	if (Functions.isStation(req.params.id)) {
-		var lat = Station[req.params.id].latitude;
-		var lon = Station[req.params.id].longitude;
-		res.send(JSON.stringify(lat));
+		const lat = Station[req.params.id].latitude;
+        const lon = Station[req.params.id].longitude;
+
+        // build list
+        let regionDistances = [];
+
+        // loop through regions and find distance
+        for(i=0; i<avyRegions.length; i++) {
+            const distance = geolib.getDistance(
+                {
+                    latitude: lat, 
+                    longitude: lon
+                },
+                {
+                    latitude: avyRegions[i].coordinates.latitude, 
+                    longitude: avyRegions[i].coordinates.longitude
+                }
+            );
+            
+            const region = {
+                "distance": Functions.meters_to_miles(distance),
+                "center": avyRegions[i].center,
+                "name": avyRegions[i].name,
+                "id": avyRegions[i].id,
+            };           
+
+            regionDistances.push(region);
+        }
+    
+        regionDistances.sort(function (a, b) {
+            return a.distance - b.distance;
+        })
+
+        const finalRegions = [];
+
+        for (i=0; i<10; i++) {
+            finalRegions.push(regionDistances[i]);
+        }
+
+        const avyReport = await avyLookup();
+
+		res.send(finalRegions);
 
 	} else {
 		res.send('please enter station')
